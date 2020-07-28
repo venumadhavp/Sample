ba6d9e8b6d68d5a7d4c9c8a668076d07609246bc
Venu8931 <58421582+Venu8931@users.noreply.github.com>
2020-07-28 12:38:02 +0530

    Modified the Test class

--- primiti-injection/src/main/java/com/pi/test/Test.java
+++ primiti-injection/src/main/java/com/pi/test/Test.java
@@ -7,7 +7,9 @@ import org.springframework.core.io.ClassPathResource;
 import com.pi.beans.Account;
 
 public class Test {
+	public static voic main(String []args){
 	BeanFactory factory= new XmlBeanFactory(new ClassPathResource("application-context.xml"));
 	Account account = factory.getBean("account", Account.class);
-
+		System.out.println(account);
+	}
 }
